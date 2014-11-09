3e249580aa95bdfed1b731c363a19fe139f9bbfc
s10n <a@akaiv.com>
2014-11-09 19:08:37 +0900

    admin

---
+++ admin/sample.php
@@ -0,0 +1,21 @@
+<h1>관리자 메뉴 샘플</h1>
+<button class="btn btn-primary" data-toggle="modal" data-target="#demoModal">Launch demo modal</button>
+
+<!-- Modal -->
+<div class="modal fade" id="demoModal" tabindex="-1" role="dialog" aria-labelledby="demoModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h4 class="modal-title" id="demoModalLabel">Modal title</h4>
+			</div>
+			<div class="modal-body">
+				...
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+	</div>
+</div>
--- inc/admin.php
+++ inc/admin.php
@@ -1,4 +1,28 @@
 <?php
+/* 관리자 페이지에 메뉴 항목 추가 */
+function akaiv_admin_menu() {
+  $icon = 'dashicons-media-code';
+  add_menu_page( '관리자 메뉴 샘플', '관리자 메뉴 샘플', 'switch_themes', 'akaiv-admin-menu-sample', function () {
+    include_once plugin_dir_path( __FILE__ ) . 'admin/sample.php';
+  }, $icon );
+}
+add_action( 'admin_menu', 'akaiv_admin_menu' );
+
+/* admin_body_class */
+function add_admin_body_class( $classes ) {
+  return "$classes my_class";
+}
+add_filter( 'admin_body_class', 'add_admin_body_class' );
+
+/* 관리자 페이지에 커스텀 스타일시트 추가 */
+function akaiv_admin_scripts( $hook ) {
+  if ( preg_match( '/akaiv/', $hook ) ) {
+    wp_enqueue_style( 'admin', get_template_directory_uri() . '/admin-style.css' );
+    wp_enqueue_script( 'admin', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
+  }
+}
+add_action( 'admin_enqueue_scripts', 'akaiv_admin_scripts' );
+
 /* 포스트 기능 제거 */
 function akaiv_init() {
   remove_post_type_support( 'post', 'author' );
