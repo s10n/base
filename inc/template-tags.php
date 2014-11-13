a8fba4624b739a4ea67b5f39b8882dfe210d3c10
s10n <a@akaiv.com>
2014-11-13 17:11:40 +0900

    템플릿태그: 페이지네이션에서 '이전', '다음' 텍스트를 제거

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -163,6 +163,7 @@ function akaiv_paginate_links() {
       'current' => max( 1, get_query_var('paged') ),
       'total' => $wp_query->max_num_pages,
       'after_page_number' => '<span class="screen-reader-text">번째 페이지</span>',
+      'prev_next' => False,
       'type' => 'array'
     );
     $paginate_links = paginate_links( $args );
