c677e226f0c258d420bce7fb6d45ea6f95f1a540
s10n <a@akaiv.com>
2014-11-16 00:59:42 +0900

    [구조 변경] functions.php: enqueue.php로부터 <head> 관련 함수들을 별개 파일로 분리

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'inc/class.php';
 require_once 'inc/enqueue.php';
+require_once 'inc/head.php';
 require_once 'inc/init.php';
 require_once 'inc/post.php';
 require_once 'inc/template-tags.php';
