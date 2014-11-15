cd4b8349ba1570fbd56249fc754ec6700512f7ce
s10n <a@akaiv.com>
2014-11-16 02:01:30 +0900

    functions.php: 오픈그래프 메타 태그들 고도화

--- inc/head.php
+++ inc/head.php
@@ -25,28 +25,72 @@ function akaiv_wp_title( $title, $sep ) {
 add_filter( 'wp_title', 'akaiv_wp_title', 10, 2 );
 
 /* <head>: 파비콘, 메타 */
+function akaiv_add_opengraph_namespace( $input ) {
+  return $input.' prefix="og: http://ogp.me/ns#"';
+}
+add_filter( 'language_attributes', 'akaiv_add_opengraph_namespace' );
 function akaiv_head() { ?>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon.png">
 
-  <?php if ( is_singular() ) : ?>
-  <meta property="og:title" content="<?php single_post_title(); ?>">
-  <meta property="og:description" content="<?php echo strip_tags(get_the_excerpt()); ?>">
-  <meta property="og:url" content="<?php the_permalink(); ?>">
-  <meta property="og:type" content="article">
-  <?php
-    $fb_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
-    if ($fb_image) :
-      echo '<meta property="og:image" content="'.$fb_image[0].'">';
+  <!-- 검색엔진최적화 - http://simcheolhwan.com -->
+  <?php $fb_image = get_template_directory_uri().'/images/fb-image.jpg'; ?>
+
+  <?php if ( is_404() ) : ?>
+    <meta name="robots" content="noindex,follow">
+    <meta property="og:title" content="<?php akaiv_title(); ?>">
+    <meta property="og:type" content="object">
+
+  <?php elseif ( is_search() ) : ?>
+    <meta name="robots" content="noindex,follow">
+    <link rel="canonical" href="<?php akaiv_url(); ?>">
+    <meta property="og:title" content="<?php akaiv_title(); ?>">
+    <meta property="og:url" content="<?php akaiv_url(); ?>">
+    <meta property="og:type" content="object">
+
+  <?php elseif ( is_archive() ) : ?>
+    <link rel="canonical" href="<?php akaiv_url(); ?>">
+    <meta property="og:title" content="<?php akaiv_title(); ?>">
+    <meta property="og:url" content="<?php akaiv_url(); ?>">
+    <meta property="og:type" content="object">
+
+  <?php elseif ( is_singular() ) : ?>
+    <meta property="og:title" content="<?php akaiv_title(); ?>">
+    <meta property="og:url" content="<?php akaiv_url(); ?>">
+    <meta property="og:type" content="article"><?php
+    if ( is_single() ) :
+      $excerpt = strip_tags(get_the_excerpt());
+      $section = get_the_category()[0]->cat_name;
+      $tags    = get_the_tags();
+      if ( $excerpt ) : ?>
+        <meta name="description" content="<?php echo $excerpt; ?>">
+        <meta property="og:description" content="<?php echo $excerpt; ?>"><?php
+      endif; ?>
+      <meta property="article:section" content="<?php echo $section; ?>">
+      <?php foreach ($tags as $tag) : ?>
+        <meta property="article:tag" content="<?php echo $tag->name; ?>">
+      <?php endforeach;
     endif;
-  ?>
+    $author_id     = get_queried_object()->post_author;
+    $author        = get_the_author_meta( 'display_name', $author_id );
+    $time          = esc_attr( get_the_date( 'c' ) );
+    $thumbnail_src = akaiv_get_post_thumbnail_src();
+    $image         = ( $thumbnail_src ) ? $thumbnail_src : $fb_image; ?>
+    <meta property="article:published_time" content="<?php echo $time; ?>">
+    <meta property="article:author" content="<?php echo $author; ?>">
 
   <?php else : ?>
-  <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>">
-  <meta property="og:description" content="<?php bloginfo( 'description' ); ?>">
-  <meta property="og:type" content="website">
-  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/fb-image.jpg">
+    <link rel="canonical" href="<?php akaiv_url(); ?>">
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+    <meta property="og:description" content="<?php bloginfo( 'description' ); ?>">
+    <meta property="og:type" content="website">
 
-  <?php endif;
+  <?php endif; ?>
+
+  <?php if ( ! is_singular() ) $image = $fb_image; ?>
+  <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>">
+  <meta property="og:image" content="<?php echo $image; ?>">
+  <meta property="og:locale" content="<?php echo bloginfo( 'language' ); ?>">
+  <!-- / 검색엔진최적화 --><?php
 }
 add_action('wp_head', 'akaiv_head');
 
