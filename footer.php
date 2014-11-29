b72768ec17a496e1981561c71165d93e55b43770
s10n <a@akaiv.com>
2014-11-29 13:59:19 +0900

    footer.php: 내용 편집

--- footer.php
+++ footer.php
@@ -4,14 +4,12 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="container">
     <div class="site-info">
-      <?php $admin = get_users( array( 'role' => 'administrator' ) )[0]; ?>
       <p class="pull-left">
-        <?php echo $admin->display_name; ?> &copy; <?php echo date( 'Y', current_time( 'timestamp' ) ); ?>
+        <a data-toggle="tooltip" data-placement="top" title="아카이브" href="http://akaiv.com/" target="_blank">akaiv</a>
+        &copy; <?php echo date( 'Y', current_time( 'timestamp' ) ); ?>
       </p>
       <p class="pull-right">
-        <a data-toggle="tooltip" data-placement="top" title="아카이브" href="http://akaiv.com/" target="_blank">akaiv</a>
-        <a class="link-admin" href="<?php echo get_admin_url(); ?>" target="_blank"><i class="fa fa-smile-o"></i></a>
-        <a data-toggle="tooltip" data-placement="top" title="Proudly Powered by WordPress" href="http://wordpress.org/" target="_blank">WordPress</a>
+        <a class="link-admin" href="<?php echo get_admin_url(); ?>" target="_blank"><i class="fa fa-fw fa-terminal"></i></a>
       </p>
     </div><!-- .site-info -->
   </div><!-- .container -->
