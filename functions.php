cc7e264788f684694d4de442b34137754b79c65b
s10n <a@akaiv.com>
2016-05-16 02:22:19 +0900

    함수: 새 프로젝트를 위한 함수 파일 생성

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+require get_template_directory() . '/inc/_project.php';
 require get_template_directory() . '/inc/class.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/head.php';
