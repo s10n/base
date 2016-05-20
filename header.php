74093ef7f01fdc0a743e07f588b00c6162993d95
s10n <a@akaiv.com>
2016-05-21 00:37:08 +0900

    템플릿: <head> 청소 및 브라우저 경고 추가

--- header.php
+++ header.php
@@ -3,18 +3,13 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-  <link rel="profile" href="http://gmpg.org/xfn/11">
-  <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-  <?php endif; ?>
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> &mdash; 피드" href="<?php echo esc_url( get_feed_link() ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<!--[if IE]><div class="container"><div class="alert alert-warning">지원하지 않는 브라우저입니다. 브라우저를 <a href="http://www.whatbrowser.org/intl/ko">업그레이드</a>하세요.</div></div><![endif]-->
-<a class="skip-link sr-only" href="#content"><?php echo 'Skip to content'; ?></a>
+<!--[if IE]><div class="container"><div class="alert alert-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>지원하지 않는 브라우저입니다. 브라우저를 <a href="http://www.whatbrowser.org/intl/ko">업그레이드</a>하세요.</div></div><![endif]-->
+<a class="sr-only sr-only-focusable" href="#content">콘텐츠로 건너뛰기</a>
 
 <header id="masthead" class="site-header" role="banner">
   <nav id="gnb" class="site-navigation gnb navbar navbar-default navbar-fixed-top" role="navigation" aria-label="메뉴">
