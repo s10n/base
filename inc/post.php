591f57c0a20ecb34c69cd331d783d003b8512787
s10n <a@akaiv.com>
2016-05-30 22:03:45 +0900

    코드 개선

--- inc/post.php
+++ inc/post.php
@@ -25,7 +25,7 @@ function akaiv_get_title() {
 
 /* 썸네일 */
 function akaiv_post_thumbnail() {
-  if ( post_password_required() && ! is_singular() ) : /* 비밀 글 */ ?>
+  if ( post_password_required() ) : /* 비밀 글 */ ?>
     <a class="post-thumbnail" href="<?php the_permalink(); ?>">
       <?php akaiv_the_post_thumbnail_placeholder( 'thumbnail', 'thumbnail-lock' ); ?>
     </a><?php
